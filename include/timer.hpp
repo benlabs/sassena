a1cfa386e5868addaed941e3177d9679efe0d616
Benjamin Lindner <benlin@benmac.(none)>
2009-06-04 00:13:20 +0200

    adding missing files...

---
+++ include/timer.hpp
@@ -0,0 +1,59 @@
+/*
+ *  timer.hpp
+ *
+ *  Created on: May 26, 2008
+ *  Authors:
+ *  Benjamin Lindner, ben@benlabs.net
+ *
+ *  Copyright 2008,2009 Benjamin Lindner
+ *
+ */
+
+#ifndef TIMER_HPP_
+#define TIMER_HPP_
+
+// common header
+#include "common.hpp"
+
+// standard header
+#include <complex>
+#include <map>
+#include <string>
+#include <sys/time.h>
+#include <vector>
+
+// special library headers
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics.hpp>
+
+// other headers
+#include "atomselection.hpp"
+#include "coor3d.hpp"
+#include "sample.hpp"
+
+using namespace boost::accumulators;
+
+class Timer {
+	typedef boost::accumulators::accumulator_set<double,features<tag::min,tag::max,tag::mean,tag::variance,tag::sum,tag::count> > times_type;
+	std::map<std::string,times_type > times;
+	
+	std::map<std::string,timeval> starttimes;
+	std::map<std::string,bool> states;
+	
+	double t_diff(timeval start, timeval end);
+public:
+	
+	std::vector<std::string> keys();
+	
+	void start(std::string tk);
+	void stop(std::string tk);
+
+	double sum(std::string tk);
+	double min(std::string tk);
+	double max(std::string tk);	
+	double mean(std::string tk);
+	double variance(std::string tk);	
+	double count(std::string tk);		
+};
+
+#endif TIMER_HPP_
---
+++ src/timer.cpp
@@ -0,0 +1,67 @@
+#include "timer.hpp"
+
+#include <deque>
+
+using namespace std;
+
+double Timer::t_diff(timeval start, timeval end) {
+	int seconds = (end.tv_sec-start.tv_sec);
+	int microseconds = (end.tv_usec-start.tv_usec);
+	seconds = (microseconds < 0) ? seconds-1 : seconds;
+	microseconds = (microseconds < 0) ? 1000000+microseconds : microseconds;
+	return seconds + (microseconds/1000000.0);
+}
+
+void Timer::start(std::string tk)  { 
+	states[tk]=true;
+
+	timeval t;
+	gettimeofday(&t, 0);
+	starttimes[tk] = t;
+}
+
+void Timer::stop(std::string tk)   { 
+	if (states.find(tk)==states.end()) return; 
+	if (states[tk]) { 
+		states[tk]=false;
+		timeval t;
+		gettimeofday(&t, 0);
+		times[tk](t_diff(starttimes[tk],t));
+	}
+	else {
+		cerr << "ERROR>> " << " Timer not started, but stopped: " << tk << endl;
+		throw;
+	}
+}
+
+vector<string> Timer::keys() {
+	vector<string> ret;
+	for (map<string,times_type>::iterator ti=times.begin();ti!=times.end();ti++) {
+		ret.push_back(ti->first);
+	}
+	return ret;
+}
+
+double Timer::mean(std::string tk) {
+	return boost::accumulators::mean(times[tk]);
+}
+
+double Timer::variance(std::string tk) {
+	return boost::accumulators::variance(times[tk]);
+}
+
+double Timer::sum(std::string tk) {
+	return boost::accumulators::sum(times[tk]);
+}
+double Timer::min(std::string tk) {
+	return boost::accumulators::min(times[tk]);
+}
+double Timer::max(std::string tk) {
+	return boost::accumulators::max(times[tk]);
+}
+
+double Timer::count(std::string tk) {
+	return boost::accumulators::count(times[tk]);
+}
+
+// end of file
