332d97c2e081b108aa3e85d04a866b84b2b8412c
Benjamin Lindner <benlin@benjamin-lindners-macbook-pro.local>
2009-09-14 23:43:40 -0400

    missing files

---
+++ include/vector_unfold.hpp
@@ -0,0 +1,97 @@
+/*
+ *  vector_unfold.hpp
+ *
+ *  Created on: Dec 30, 2008
+ *  Authors:
+ *  Benjamin Lindner, ben@benlabs.net
+ *
+ *  Copyright 2008,2009 Benjamin Lindner
+ *
+ */
+
+#ifndef VECTOR_UNFOLD_HPP_
+#define VECTOR_UNFOLD_HPP_
+
+// common header
+#include "common.hpp"
+
+// standard header
+#include <string>
+#include <vector>
+
+// special library headers
+
+// other headers
+#include "atomselection.hpp"
+#include "sample.hpp"
+#include "frames.hpp"
+#include "coordinate_set.hpp"
+
+class VectorUnfold {
+protected:
+	CartesianCoor3D m_q;
+	std::vector<CartesianCoor3D> m_qvectors;	
+public:
+	virtual void execute() = 0;
+	virtual std::vector<CartesianCoor3D>& qvectors() = 0;	
+};
+
+class NoVectorUnfold : public VectorUnfold {
+public:
+	NoVectorUnfold(CartesianCoor3D q);
+	
+	void execute();
+	std::vector<CartesianCoor3D>& qvectors();	
+};
+
+class SphereVectorUnfold : public VectorUnfold {
+	long m_seed;
+	size_t m_resolution;
+	std::string m_vectors;
+public:
+	SphereVectorUnfold(CartesianCoor3D q);
+	
+	void execute();
+	std::vector<CartesianCoor3D>& qvectors();	
+	
+	void set_resolution(size_t resolution);
+	void set_seed(long seed);
+	void set_vectors(std::string vectors);
+};
+
+class CylinderVectorUnfold : public VectorUnfold {
+	CartesianCoor3D m_axis;
+	long m_seed;
+	size_t m_resolution;
+	std::string m_vectors;
+public:
+	CylinderVectorUnfold(CartesianCoor3D q);
+	
+	void execute();
+	std::vector<CartesianCoor3D>& qvectors();
+	
+	void set_resolution(size_t resolution);
+	void set_seed(long seed);
+	void set_vectors(std::string vectors);
+	void set_axis(CartesianCoor3D axis);
+};
+
+// we can actually support tons of different unfolding schemes:
+class GaussianVectorUnfold : public VectorUnfold {
+	
+};
+
+// this reads vectors from a file. Only their direction is used to generate the final vectors
+class FileVectorUnfold : public VectorUnfold {
+public:
+	FileVectorUnfold(CartesianCoor3D q);
+	
+	void execute();
+	std::vector<CartesianCoor3D>& qvectors();
+	
+};
+
+
+#endif
+
+// end of file
